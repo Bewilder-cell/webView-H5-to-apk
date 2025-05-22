3e03f816af81113e2b382a480ac70b29e087a185
bewilder-cell <1761877578@qq.com>
2025-05-23 00:43:29 +0800

    Update CrashHandler.java

--- app/src/main/java/com/example/test/CrashHandler.java
+++ app/src/main/java/com/example/test/CrashHandler.java
@@ -9,28 +9,23 @@ import android.util.Log;
 
 public class CrashHandler implements Thread.UncaughtExceptionHandler {
     private static final String TAG = "CrashHandler";
-    private Context context;
-    private Thread.UncaughtExceptionHandler defaultHandler;
+    private final Context context;
+    private final Thread.UncaughtExceptionHandler defaultHandler;
 
     public CrashHandler(Context context) {
-        this.context = context;
-        // 保存默认的异常处理器
-        defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        this.context = context.getApplicationContext(); // 避免内存泄漏
+        this.defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
     }
 
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
         Log.e(TAG, "App crashed, restarting...", ex);
-        
-        // 重启应用
         restartApp();
-        
-        // 如果有默认的异常处理器，也让它处理
+
         if (defaultHandler != null) {
             defaultHandler.uncaughtException(thread, ex);
         }
-        
-        // 结束进程
+
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
     }
@@ -39,8 +34,7 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         try {
             Intent intent = new Intent(context, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            
-            // 使用AlarmManager延迟1秒重启应用
+
             PendingIntent pendingIntent;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 pendingIntent = PendingIntent.getActivity(context, 0, intent,
@@ -52,10 +46,14 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
 
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
             if (alarmManager != null) {
-                alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 1000, pendingIntent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
+                } else {
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
+                }
             }
         } catch (Exception e) {
             Log.e(TAG, "重启应用失败", e);
         }
     }
-} 
+}
