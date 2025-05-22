102d3897372c1b06fcef32b7197c133894e1d106
bewilder-cell <1761877578@qq.com>
2025-05-22 23:51:37 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -111,7 +111,12 @@ if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 handleup(filePathCallback);
                 return true;
             }
-
+private void restartApp() {
+    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    getApplicationContext().startActivity(intent);
+    android.os.Process.killProcess(android.os.Process.myPid()); // 杀掉原进程
+}
             private void handle(ValueCallback<Uri> uploadFile) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 // 设置允许上传的文件类型
@@ -177,6 +182,12 @@ if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 view.loadUrl(url);
                 return true;
             }
+                // 渲染进程崩溃（Android 7.0+）
+    @Override
+    public void onRenderProcessGone(WebView view, RenderProcessGoneDetail detail) {
+        Log.e("WebView", "渲染进程崩溃，是否被杀: " + detail.didCrash());
+        restartApp(); // WebView 崩溃时重启
+    }
         });
         // 获取当前时间戳
 long timestamp = System.currentTimeMillis();
