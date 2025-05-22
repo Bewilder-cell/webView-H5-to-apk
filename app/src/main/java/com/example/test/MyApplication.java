3a357d95c268014fd7ec52615f9da5d5625444e4
bewilder-cell <1761877578@qq.com>
2025-05-22 21:18:45 +0800

    Update MyApplication.java

--- app/src/main/java/com/example/test/MyApplication.java
+++ app/src/main/java/com/example/test/MyApplication.java
@@ -6,5 +6,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+           // 注册崩溃处理器
+        Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(getApplicationContext()));
     }
 } 
