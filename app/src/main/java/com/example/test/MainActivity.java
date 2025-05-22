ab0bb6194a640859cd468533ad7c7ff0a1271d61
bewilder-cell <1761877578@qq.com>
2025-05-22 13:48:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -145,8 +145,8 @@ public class MainActivity extends AppCompatActivity {
 long timestamp = System.currentTimeMillis();
 
 // 这里填你需要打包的 H5 页面链接，并附加时间戳参数
-// String url = "http://172.16.102.55:8082/#/";
-        String url = "https://www.baidu.com";
+ String url = "http://172.16.102.55:8082/#/";
+        // String url = "https://www.baidu.com";
         // 这里填你需要打包的 H5 页面链接
         webView.loadUrl(url);
 
