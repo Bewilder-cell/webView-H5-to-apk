56cc1cd15f9ac838c91d532c26b5175ae8ccac15
bewilder-cell <1761877578@qq.com>
2025-06-10 10:44:44 +0800

    1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         });
         
         // 加载URL
-        String url = "http://192.168.6.212:8105";
+        String url = "http://192.168.6.212:8106";
         webView.loadUrl(url);
     }
 
