71a225b56633666d93a27805c38e2e6b3142d329
bewilder-cell <1761877578@qq.com>
2025-06-19 16:54:30 +0800

    1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        String url = "http://10.114.136.173:8082/#/pages/views/pickStateNewTW";
+        String url = "http://10.114.136.173:8082";
         webView.loadUrl(url);
     }
 
