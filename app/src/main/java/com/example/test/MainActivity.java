a53c6bde8bcc8ef546ef3ee2ba295dcb024aebdf
bewilder-cell <1761877578@qq.com>
2025-07-08 10:00:00 +0800

    1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         });
         // GS-Report分享链接：http://172.16.10.10:8090/#/aj/AsNZYR0s
         // 加载URL
-        String url = "http://172.16.10.10:8090/#/aj/AsNZYR0s";
+        String url = "http://172.16.10.10:8090/#/aj/wcwFX3FH";
         webView.loadUrl(url);
     }
 
