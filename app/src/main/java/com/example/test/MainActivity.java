54110b24d7d7a40830412199edf5a2ecd17782a5
bewilder-cell <1761877578@qq.com>
2025-11-01 16:30:28 +0800

    1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -144,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
         
 
         // 这里填你需要打包的 H5 页面链接
-        webView.loadUrl("http://172.16.68.51:8091");
+        webView.loadUrl("http://172.16.68.5/:8090/#/aj/5PQQkLoR");
 
         //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
